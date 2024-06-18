17109d847d1a9c1228a9048e3af87268b0372c59
MatveiStudentIITK <questus.scientia@list.ru>
2024-06-18 14:33:32 +0700

    Добавьте файлы проекта.

---
+++ ifcCheckerRules/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+
+namespace ifcCheckerRules
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static XmlDocument ifcCheck(FileStream IFCFileStream, XmlDocument XMLRulesFile)
+        {
+
+
+            return new XmlDocument();
+        }
+    }
+}
